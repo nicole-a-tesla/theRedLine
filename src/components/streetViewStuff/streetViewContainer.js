ae469ddd183aa95c87b79ba4a41f60d5fc7bb4de
nicole-a-tesla <thisisnotnicole@gmail.com>
2015-07-07 12:53:30 -0500

    more debugging code removal

--- src/components/streetViewStuff/streetViewContainer.js
+++ src/components/streetViewStuff/streetViewContainer.js
@@ -1,14 +1,9 @@
-
 var React = require('react');
-var StreetViewsByStop = require('./streetViewsByStop');
 var StreetViewImage = require('./streetViewImage');
 
 var StreetViewContainer = React.createClass({
 
   render: function() {
-    console.log('rendering streetViewContainer');
-    // console.log('this.props.data.lat = ');
-    // console.log(this.props.data.lat);
     return (
       <StreetViewImage lat={this.props.data.lat} long={this.props.data.long} />
     );
--- src/components/streetViewStuff/streetViewImage.js
+++ src/components/streetViewStuff/streetViewImage.js
@@ -3,14 +3,10 @@ var React = require('react');
 var StreetViewImage = React.createClass({
 
   getInitialState: function() {
-    // console.log('getting initial state for image...');
     return {imgUrl: 'https://maps.googleapis.com/maps/api/streetview?size=400x400&location=' + this.props.lat + ',' + this.props.long + '&pitch=-0.76'}
   },
 
   render: function() {
-    console.log('rendering streetViewImage');
-    // console.log('imgUrl is ');
-    // console.log(this.state.imgUrl);
     return (
       <img src={this.state.imgUrl}></img>
     )
