dd985c5cdcf90a3cecf11719491355399e59b823
nicole-a-tesla <thisisnotnicole@gmail.com>
2015-07-10 12:35:21 -0500

    centered map on loop

--- src/components/mapStuff/map.js
+++ src/components/mapStuff/map.js
@@ -2,7 +2,12 @@ var React = require('react');
 
 var Map = React.createClass({
   componentDidMount: function() {
-    cartodb.createVis('map', 'https://nicole-a-tesla.cartodb.com/api/v2/viz/0713289c-2682-11e5-9584-0e49835281d6/viz.json');
+    var url = 'https://nicole-a-tesla.cartodb.com/api/v2/viz/0713289c-2682-11e5-9584-0e49835281d6/viz.json'
+    cartodb.createVis('map', url, {
+      center_lat: 41.884809,
+      center_lon: -87.627813
+    });
+
   },
   render: function() {
     return (
