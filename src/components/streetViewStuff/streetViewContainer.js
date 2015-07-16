79759b9d7e3dbf6e9fe8d1f1efbb829bb4453b26
nicole-a-tesla <thisisnotnicole@gmail.com>
2015-07-16 16:40:21 -0500

    street view working

--- src/components/streetViewStuff/streetViewContainer.js
+++ src/components/streetViewStuff/streetViewContainer.js
@@ -4,6 +4,9 @@ var StreetViewImage = require('./streetViewImage');
 var StreetViewContainer = React.createClass({
 
   render: function() {
+    console.log('------------------------');
+    console.log('in StreetViewContainer');
+    console.log('------------------------');
     return (
       <StreetViewImage lat={this.props.data.lat} long={this.props.data.long} />
     );
