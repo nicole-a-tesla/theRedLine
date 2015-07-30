0804e0ebb9889fa4e7048a8b859f112f62a3b275
nicole-a-tesla <thisisnotnicole@gmail.com>
2015-07-30 13:52:35 -0500

    set up bones of new incomeVis component, remaned data file incomeData

---
+++ src/components/dataVis/incomeVis.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var incomeData = require('./income')
+
+
+var IncomeVis = React.createClass({
+  render: function() {
+    console.log('INCOME VIS -->');
+    console.log(this.props.data.station_name);
+    return (
+
+    );
+  }
+});
+
+module.exports = incomeVis;
+// <script type="text/javascript">
+//   var dataset = income[this.props.station_name];
+//
+// </script>
--- src/components/trainStation.js
+++ src/components/trainStation.js
@@ -3,6 +3,7 @@ var StreetViewContainer = require('./streetViewStuff/streetViewContainer');
 var InstagramContainer = require('./instagramStuff/instagramContainer');
 var ctaStops =  require('./ctaStops');
 var $ = require('jquery');
+var IncomeVis = require('./dataVis/incomeVis')
 
 var TrainStation = React.createClass({
 
@@ -16,6 +17,7 @@ var TrainStation = React.createClass({
       <div className='trainStation' id={ctaStops[this.props.position].station_name}>
         <StreetViewContainer className={'streetViewContainer'} data={ctaStops[this.props.position]} />
         <InstagramContainer className={'instagramContainer'} data={ctaStops[this.props.position]} />
+        <IncomeVis ctaData={ctaStops[this.props.position]} />
       </div>
     );
   }
