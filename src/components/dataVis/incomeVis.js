b4b10b547a3762d2c68c3b6b606eeb04cb5ee44f
nicole-a-tesla <thisisnotnicole@gmail.com>
2015-07-30 14:56:14 -0500

    put in the d3 code, no dice yet

--- src/components/dataVis/incomeVis.js
+++ src/components/dataVis/incomeVis.js
@@ -6,18 +6,36 @@ var IncomeVis = React.createClass({
 
   render: function() {
 
-    // console.log('INCOME VIS -->');
-    // console.log(income[this.props.data.station_name]);
+    console.log('INCOME VIS -->');
+    // console.log(incomeData[this.props.data.neighborhood]);
     return (
       <div>
         <p>Hello!</p>
+        <script type="text/javascript">
+          var svgW = 100;
+          var svgH = 100;
+          var padding = 0;
+
+          var dataset = incomeData[this.props.data.neighborhood];
+
+          var svg = d3.select('div')
+                      .append('svg')
+                      .attr('width', w)
+                      .attr('height', h);
+
+          svg.selectAll('circle')
+              .data(dataset)
+              .enter()
+              .append('circle')
+              .attr('cx', 50)
+              .attr('cy', 50)
+              .attr('r', function(d) {
+                return d /10; // React is all kinds of :( about this return
+              });
+        </script>
       </div>
     )
   }
 });
 
 module.exports = IncomeVis;
-// <script type="text/javascript">
-//   var dataset = income[this.props.station_name];
-//
-// </script>
