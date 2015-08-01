2f1c8d4a3b2876478442a515691764c977c19416
nicole-a-tesla <thisisnotnicole@gmail.com>
2015-08-01 10:24:59 -0500

    link to google maps in index and setting up panorama streetview

--- src/components/streetViewStuff/streetViewImage.js
+++ src/components/streetViewStuff/streetViewImage.js
@@ -1,11 +1,61 @@
 var React = require('react');
 
 var StreetViewImage = React.createClass({
+
+  loadStreetView: function() {
+    var trainStation = new google.maps.LatLng(this.props.lat, this.props.long);
+    var panoramaOptions = {
+      position: trainStation,
+      pov: {
+        heading: 165,
+        pitch: 0
+      },
+      zoom: 1
+    };
+
+    var myPano = new google.maps.StreetViewPanorama(
+      document.getElementById('streetview-canvas'),
+      panoramaOptions);
+    myPano.setVisible(true);
+  },
+
+  componentDidMount: function() {
+    this.loadStreetView();
+  },
+
   render: function() {
     return (
-      <img src={'https://maps.googleapis.com/maps/api/streetview?size=700x200&location=' + this.props.lat + ',' + this.props.long + '&pitch=-0.76'}></img>
-    )
+      <div id="streetview-canvas"></div>
+    );
   }
+
 });
 
+
+//
+// function initialize() {
+//   var bryantPark = new google.maps.LatLng(37.869260, -122.254811);
+//   var panoramaOptions = {
+//     position: bryantPark,
+//     pov: {
+//       heading: 165,
+//       pitch: 0
+//     },
+//     zoom: 1
+//   };
+//   var myPano = new google.maps.StreetViewPanorama(
+//       document.getElementById('map-canvas'),
+//       panoramaOptions);
+//   myPano.setVisible(true);
+// }
+//
+// google.maps.event.addDomListener(window, 'load', initialize);
+
+
+//   render: function() {
+//     return (
+//       <img src={'https://maps.googleapis.com/maps/api/streetview?size=700x200&location=' + this.props.lat + ',' + this.props.long + '&pitch=-0.76'}></img>
+//     )
+//   }
+// });
 module.exports = StreetViewImage;
