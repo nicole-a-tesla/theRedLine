5a1ae44b2ff77806c28f229caf66108d7e600f0c
nicole-a-tesla <thisisnotnicole@gmail.com>
2015-08-04 01:53:06 -0500

    fixed racedata format

--- src/components/raceData.js
+++ src/components/raceData.js
@@ -1,100 +1,100 @@
 module.exports={
-  'Rogers Park': {
+  'Rogers Park': [
     ['White', 39.31],
     ['Black',  26.3],
     ['Latin@', 24.43],
     ['Asian', 6.41],
     ['Other', 3.56]
-  },
-  'Edgewater': {
+  ],
+  'Edgewater': [
     ['White', 54.65],
     ['Black', 14.34],
     ['Latin@', 16.49],
     ['Asian', 11.65],
     ['Other', 2.88]
-  },
-  'Uptown': {
+  ],
+  'Uptown': [
     ['White', 51.63],
     ['Black', 20.0],
     ['Latin@', 14.21],
     ['Asian', 11.38],
     ['Other', 2.78]
-  },
-  'Lakeview': {
+  ],
+  'Lakeview': [
     ['White', 80.37],
     ['Black', 3.87],
     ['Latin@', 7.63],
     ['Asian', 5.99],
     ['Other', 2.14]
-  },
-  'Lincoln Park': {
+  ],
+  'Lincoln Park': [
     ['White', 82.88],
     ['Black', 4.29],
     ['Latin@', 5.57],
     ['Asian', 5.14],
     ['Other', 2.12]
-  },
-  'Near North Side': {
+  ],
+  'Near North Side': [
     ['White', 72.11],
     ['Black', 10.85],
     ['Latin@', 4.94],
     ['Asian', 10.11],
     ['Other', 1.99]
-  },
-  'Loop': {
+  ],
+  'Loop': [
     ['White', 62.7],
     ['Black', 11.48],
     ['Latin@', 6.86],
     ['Asian', 15.91],
     ['Other', 3.06]
-  },
-  'Near South Side': {
+  ],
+  'Near South Side': [
     ['White', 48.06],
     ['Black', 28.12],
     ['Latin@', 5.62],
     ['Asian', 15.46],
     ['Other', 2.73]
-  },
-  'Armour Square': {
+  ],
+  'Armour Square': [
     ['White', 12.26],
     ['Black', 10.6],
     ['Latin@', 3.47],
     ['Asian', 72.59],
     ['Other', 1.08]
-  },
-  'Fuller Park': {
+  ],
+  'Fuller Park': [
     ['White', 1.63],
     ['Black', 92.18],
     ['Latin@', 4.59],
     ['Asian', 0.24],
     ['Other', 1.36]
-  },
-  'Englewood': {
+  ],
+  'Englewood': [
     ['White', 0.34],
     ['Black', 97.37],
     ['Latin@', 1.06],
     ['Asian', 0.11],
     ['Other', 1.12]
-  },
-  'Greater Grand Crossing': {
+  ],
+  'Greater Grand Crossing': [
     ['White', 0.55],
     ['Black', 96.9],
     ['Latin@', 1.19],
     ['Asian', 0.06],
     ['Other', 1.3]
-  },
-  'Chatham': {
+  ],
+  'Chatham': [
     ['White', 0.36],
     ['Black', 97.2],
     ['Latin@', 1.00],
     ['Asian', 0.09],
     ['Other', 1.35]
-  },
-  'Roseland': {
+  ],
+  'Roseland': [
     ['White', 0.42],
     ['Black', 97.37],
     ['Latin@', 1.03],
     ['Asian', 0.06],
     ['Other', 1.12]
-  },
+  ],
 }
