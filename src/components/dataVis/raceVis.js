d794cee7793ee06746b26737f4387504f431abf5
nicole-a-tesla <thisisnotnicole@gmail.com>
2015-08-17 14:24:27 -0500

    fixed over scroll issue

--- src/components/dataVis/raceVis.js
+++ src/components/dataVis/raceVis.js
@@ -19,7 +19,7 @@ var RaceVis = React.createClass({
     var chart = c3.generate({
       size: {
         height: 250,
-        width: 500
+        width: 400
       },
       bindto: "#race",
       data: {
